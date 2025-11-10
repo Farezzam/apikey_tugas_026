5d8d0ee8e274c8768f198dd36932729e5b3abafa
ASUS <faris13abiyu@gmail.com>
2025-11-10 14:51:26 +0700

    tambah SS database

--- SS/SStugas.md
+++ SS/SStugas.md
@@ -1,3 +1,3 @@
 ![postman](<Screenshot 2025-11-10 124931.png>) 
 ![web](<Screenshot 2025-11-10 124952.png>)
-![database](<Screenshot 2025-11-10 130125.png>)
+![database](<Screenshot 2025-11-10 144940.png>)
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+ 
 
 app.post('/create', async (req, res) => {
     
